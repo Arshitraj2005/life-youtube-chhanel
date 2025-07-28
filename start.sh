15a4fe7e478ef93fa53ebc5fac72758047673654
Arshitraj2005 <nigachu789@gmail.com>
2025-07-28 11:09:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-gunicorn main:app --bind 0.0.0.0:$PORT &
 
-# Wait for Flask to initialize
-sleep 5
-
-# Start your YouTube stream
+# Directly start YouTube stream with stream.py
 python3 stream.py
