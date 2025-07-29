a013e8be4d7a2a7eb8e3a008b1ee98ccbe549222
Arshitraj2005 <nigachu789@gmail.com>
2025-07-29 21:55:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def home():
     return "✅ Flask app is running 24x7 using UptimeRobot!"
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
 def keep_alive():
     t = Thread(target=run)
