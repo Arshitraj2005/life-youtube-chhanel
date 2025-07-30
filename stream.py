2443f406aeaba1142dd72583d09ad6393b940d1c
Arshitraj2005 <nigachu789@gmail.com>
2025-07-30 20:22:11 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -4,7 +4,7 @@ import time
 import os
 
 # 🎬 Your Google Drive video ID
-drive_id = "1BxfuU9_koNnljEbyXwSCxWkrnSyOGFhU"
+drive_id = "10SB9BColV3QAgNvyBrVhjsoCkwrhpTov"
 local_file = "video.mp4"
 
 # 🔑 Your YouTube stream key (hardcoded as requested)
